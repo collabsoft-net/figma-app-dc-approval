89585ab6f629f7bc8798e15c71d59b0fcdbe9d09
Remie Bolte <remie@collabsoft.net>
2021-03-01 09:37:29 +0100

    Add selenium UI tests

--- app/extension/bitbucket/extension_ui.py
+++ app/extension/bitbucket/extension_ui.py
@@ -17,10 +17,11 @@ def app_specific_action(webdriver, datasets):
     @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_repo_page")
+        @print_timing("selenium_app_custom_action:view_gitflow_chart")
         def sub_measure():
             page.go_to_url(f"{BITBUCKET_SETTINGS.server_url}/projects/{project_key}/repos/{repo_slug}/browse")
             page.wait_until_visible((By.CSS_SELECTOR, '.aui-navgroup-vertical>.aui-navgroup-inner')) # Wait for repo navigation panel is visible
-            page.wait_until_visible((By.ID, 'ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT'))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.CSS_SELECTOR, 'a[data-web-item-key="net.collabsoft.plugins.stash.gfc:graph]')).click() # Wait for repo navigation panel is visible
+            page.wait_until_visible((By.CSS_SELECTOR, "iframe[data-ac-polyfill]"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
